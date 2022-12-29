b40a56b27e0dc1ab24339f32a732789dd10969bf
KGabard <kev.gabard@gmail.com>
2022-12-29 12:08:21 +0100

    correction coquille dans convertDate

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -13,8 +13,8 @@ const convertDate = (dateString) => {
     'Mar.': '03',
     'Avr.': '04',
     'Mai.': '05',
-    'Jun.': '06',
-    'Jul.': '07',
+    'Jui.': '06',
+    'Jui.': '07',
     'Aoû.': '08',
     'Sep.': '09',
     'Oct.': '10',
@@ -33,6 +33,12 @@ const sortByDateDescending = (array) => {
     const dateB = convertDate(b.date)
     const valueDateA = new Date(dateA[0], dateA[1] - 1, dateA[2]).getTime()
     const valueDateB = new Date(dateB[0], dateB[1] - 1, dateB[2]).getTime()
+    console.log(a.date);
+    console.log(b.date);
+    console.log(dateA);
+    console.log(dateB);
+    console.log(valueDateA);
+    console.log(valueDateB);
     return valueDateB - valueDateA
   })
 }
